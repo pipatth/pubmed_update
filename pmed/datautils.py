eba04da784489f198dd6b19e8afed530f9a43108
pipatth <pipatth@users.noreply.github.com>
2019-05-06 14:45:51 -0400

    Update datautils.py

--- pmed/datautils.py
+++ pmed/datautils.py
@@ -8,19 +8,16 @@ def create_tables(conn):
     conn.execute("DROP TABLE IF EXISTS journal")
     conn.execute("DROP TABLE IF EXISTS article")
     conn.execute("DROP TABLE IF EXISTS citation")
-    conn.execute("DROP TABLE IF EXISTS citecount")
     conn.execute(
         "CREATE TABLE IF NOT EXISTS journal (id INTEGER PRIMARY KEY UNIQUE AUTO_INCREMENT, name TEXT) DEFAULT CHARSET=utf8mb4")
     conn.execute(
-        "CREATE TABLE IF NOT EXISTS article (id INTEGER PRIMARY KEY UNIQUE, title TEXT, abstract TEXT, pubyear INTEGER, jid INTEGER, keywords TEXT, pmc INTEGER) DEFAULT CHARSET=utf8mb4")
+        "CREATE TABLE IF NOT EXISTS article (id INTEGER PRIMARY KEY UNIQUE, title TEXT, abstract TEXT, pubyear INTEGER, jid INTEGER, keywords TEXT, pmc INTEGER, citations INTEGER) DEFAULT CHARSET=utf8mb4")
     conn.execute(
         "CREATE TABLE IF NOT EXISTS citation (id INTEGER PRIMARY KEY UNIQUE AUTO_INCREMENT, apmid INTEGER, bpmid INTEGER) DEFAULT CHARSET=utf8mb4")
-    conn.execute(
-        "CREATE TABLE IF NOT EXISTS citecount (id INTEGER PRIMARY KEY UNIQUE, citations INTEGER) DEFAULT CHARSET=utf8mb4")
+    conn.execute("CREATE INDEX article_pubyear_IDX USING BTREE ON article (pubyear);")
+    conn.execute("CREATE INDEX article_cite_IDX USING BTREE ON article (citations);")
     conn.execute("CREATE INDEX citation_apmid_IDX USING BTREE ON citation (apmid);")
     conn.execute("CREATE INDEX citation_bpmid_IDX USING BTREE ON citation (bpmid);")
-    conn.execute("CREATE INDEX citecount_citations_IDX USING BTREE ON citecount (citations);")
-    conn.execute("CREATE INDEX article_pubyear_IDX USING BTREE ON article (pubyear);")
     conn.execute("ALTER TABLE article ADD FULLTEXT (title);")
     conn.execute("ALTER TABLE article ADD FULLTEXT (keywords);")
     conn.execute("ALTER TABLE journal ADD FULLTEXT (name);")
@@ -70,7 +67,7 @@ def get_ids(conn):
 # get citation count
 def get_cnt(conn, id):
     # find id
-    stmt = sqlalchemy.text('''SELECT citations FROM citecount WHERE id = :i''')
+    stmt = sqlalchemy.text('''SELECT citations FROM article WHERE id = :i''')
     stmt = stmt.bindparams(i=str(id))
     result = conn.execute(stmt).fetchall()
     if result:
@@ -82,7 +79,7 @@ def get_cnt(conn, id):
 # update citation count
 def update_cnt(conn, id, cnt):
     # update by id
-    stmt = sqlalchemy.text('''UPDATE citecount SET citations = :c WHERE id = :i''')
+    stmt = sqlalchemy.text('''UPDATE article SET citations = :c WHERE id = :i''')
     stmt = stmt.bindparams(c=str(cnt), i=str(id))
     conn.execute(stmt)
     return
